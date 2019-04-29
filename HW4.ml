d912dde310684e295e52bbfa5ba9e0f1b76b082c
Christina <perrc10@mail.sunysuffolk.edu>
2019-04-29 09:48:34 -0400

    hw4

---
+++ HW4.ml
@@ -0,0 +1,19 @@
+let rec pow x n =
+    | n = 0 -> 1
+    | n = 1 -> x
+    | n*pow (b-1)
+;;
+
+let float_pow x n =
+    | n = 0 -> 1
+    | n = 1 -> x
+    | n*pow b-1
+;;
+
+let rec reverse acc = function
+    | [] -> acc
+    | h::t -> reverse (h::acc) t in
+reverse [] list;;
+
+let rec compress =
+
